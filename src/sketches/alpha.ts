7dbeae25a834550b34918ab5a7cb0c11ad4d2cc3
Jon Sherrard <jon@devular.com>
2022-10-04 23:05:18 +0100

    Basic colors

--- src/sketches/alpha.ts
+++ src/sketches/alpha.ts
@@ -3,12 +3,13 @@ import { SetupOptions } from '../App';
 
 export const setup = (p5: p5Types, options: SetupOptions) => {
   p5.createCanvas(options.width, options.height);
-  p5.background('#393E46');
+  // pastel pink background
+  p5.background('#f7c8d9');
   p5.ellipse(200, 200, 80, 80);
 };
 
 export const draw = (p5: p5Types) => {
-  p5.background('#393E46');
+  p5.background('#f7c8d9');
   const { mouseX, mouseY } = p5;
   if (mouseX > 0 && mouseY > 0) {
     p5.ellipse(mouseX, mouseY, 80, 80);
--- src/sketches/beta.ts
+++ src/sketches/beta.ts
@@ -1,14 +1,16 @@
 import p5Types from 'p5';
 import { SetupOptions } from '../App';
 
+const pastelGreen = '#c8f7d9';
+
 export const setup = (p5: p5Types, options: SetupOptions) => {
   p5.createCanvas(options.width, options.height);
-  p5.background('#bada55');
+  p5.background(pastelGreen);
   p5.ellipse(200, 200, 80, 80);
 };
 
 export const draw = (p5: p5Types) => {
-  p5.background('#bada55');
+  p5.background(pastelGreen);
   const { mouseX, mouseY } = p5;
   if (mouseX > 0 && mouseY > 0) {
     p5.ellipse(mouseX, mouseY, 80, 80);
